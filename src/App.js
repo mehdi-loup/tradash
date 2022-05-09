4a0e06f9205c377b178093e2969ff8804eb52273
mehdi-loup <mehdiloup.nasom@gmail.com>
2022-05-09 22:13:33 +0200

    add onboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import Onboard from '@web3-onboard/core';
+import injectedModule from '@web3-onboard/injected-wallets';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+const MAINNET_RPC_URL = 'https://mainnet.infura.io/v3/<INFURA_KEY>'
+
+const injected = injectedModule()
+
+const onboard = Onboard({
+  wallets: [injected],
+  chains: [
+    {
+      id: '0x1',
+      token: 'ETH',
+      label: 'Ethereum Mainnet',
+      rpcUrl: MAINNET_RPC_URL
+    }
+  ],
+  appMetadata: {
+    name: 'My App',
+    icon: '<SVG_ICON_STRING>',
+    description: 'My app using Onboard'
+  }
+})
+
+const wallets = await onboard.connectWallet()
+
+console.log(wallets)
   return (
     <div className="App">
       <header className="App-header">
